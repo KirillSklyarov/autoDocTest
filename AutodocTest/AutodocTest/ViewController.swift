42fb71a68b5208edbda84c72181e429e6e317ad2
Kirill_Sklyarov <k.sklyarov@yandex.ru>
2025-07-23 15:43:18 +0300

    feat(app): - initial commit

---
+++ AutodocTest/AutodocTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutodocTest
+//
+//  Created by Kirill Sklyarov on 23.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
